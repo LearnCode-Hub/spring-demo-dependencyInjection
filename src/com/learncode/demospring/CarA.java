fd512328e285e904b963fe0a231256dfbe40765b
LearnCode-Hub <30805468+LearnCode-Hub@users.noreply.github.com>
2020-06-27 14:51:49 +0530

    Update CarA.java

--- src/com/learncode/demospring/CarA.java
+++ src/com/learncode/demospring/CarA.java
@@ -1,14 +1,16 @@
 package com.learncode.demospring;
 
 public class CarA implements Car {
-String carName;
-String cost;
-String modelNumber;
+private String carName;
+private String cost;
+private String modelNumber;
 
+	//setting carName using Constructor
 	public CarA(String carName){
 		this.carName=carName;
 	}
 	
+	//setting cost and modelNumber using Setter method
 	public void setCost(String cost) {
 		this.cost = cost;
 	}
